f516ccff49ecf7a62946098082bb1b0c76ab2087
amiacat <cutec7782@gmail.com>
2022-05-06 18:52:43 +0000

    #3 Push

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,20 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..11);
 
-    println!("Write your guessing:")
+    println!("The secret number is {secret_number}");
+
+    println!("Put your guessing: ");
 
     let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .unwrap();
+
+    println!("You guessed {guess}");
 }
